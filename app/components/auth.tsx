81301b143836eef2cdc18c0023c037055ca64435
निखिल <59004928+Nikhil1619@users.noreply.github.com>
2025-09-12 15:06:12 +0530

    Update auth.tsx

--- app/components/auth.tsx
+++ app/components/auth.tsx
@@ -48,7 +48,6 @@ export function AuthPage() {
 
   return (
     <div className={styles["auth-page"]}>
-      <TopBanner></TopBanner>
       <div className={styles["auth-header"]}>
         <IconButton
           icon={<LeftIcon />}
